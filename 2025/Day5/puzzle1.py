83af5b1fefedbc2ba3ddbbb1c4319fdff2726cb7
Francis <francis.the.meerkat@gmail.com>
2025-12-05 10:17:13 +0200

    Now with comprehension (and incomprehensible)

--- 2025/Day5/puzzle1.py
+++ 2025/Day5/puzzle1.py
@@ -1,14 +1,8 @@
-a,b = open("puzzle1.txt").read().split('\n\n')
-a = [list(map(int, e.split('-'))) for e in a.splitlines()]
-b = [int(e) for e in b.splitlines()]
-s = set()
-
-for c in a:
-    for d in b:
-        if c[0] <= d <= c[1]:
-            s.add(d)
+a, b = open("puzzle1.txt").read().split('\n\n')
+a, b = [list(map(int, e.split('-'))) for e in a.splitlines()], [int(e) for e in b.splitlines()]
 
 
+s = set([d for c in a for d in b if c[0] <= d <= c[1]])
 p = len(s)
     
 print(p)
